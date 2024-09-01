d220d4338ff9dae59e8db05b33efca0437080210
Stiliyan Nanovski <S.I.Nanovski@student.tudelft.nl>
2024-09-01 10:00:36 +0200

    Initial commit.

---
+++ src/main/java/io/github/stelitop/battledudes/BattledudesApplication.java
@@ -0,0 +1,13 @@
+package io.github.stelitop.battledudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattledudesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattledudesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/stelitop/battledudes/BattledudesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.stelitop.battledudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattledudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
